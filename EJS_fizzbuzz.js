da86c22b720844bf8fabaaadc26e017a4b802246
Cibirsk <stvl@gmx.com>
2021-04-23 16:51:09 +0200

    Update EJS_fizzbuzz.js

--- EJS_fizzbuzz.js
+++ EJS_fizzbuzz.js
@@ -10,3 +10,11 @@ for(i=1;i<=100;i++){
         console.log(i);
     }
 }
+
+//solution du livre
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz"; 
+  if (n % 5 == 0) output += "Buzz"; //concaténation ou pas de "Fizz" et "Buzz" dans Output
+  console.log(output || n); // règle true/false avec || , si true => Fizz et/ou Buzz si false => n
+}
