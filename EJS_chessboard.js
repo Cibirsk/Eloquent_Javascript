fa5a657e057d7cb5906d72cdf1bd16f56bc57cec
Cibirsk <stvl@gmx.com>
2021-04-25 09:44:32 +0200

    Update EJS_chessboard.js

--- EJS_chessboard.js
+++ EJS_chessboard.js
@@ -19,7 +19,7 @@ let board = "";
 
 for (let y = 0; y < size; y++) {
   for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) {
+    if ((x + y) % 2 == 0) { //point clef: faire le modulo sur l'addition des 2 boucles
       board += " ";
     } else {
       board += "#";
