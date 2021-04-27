354cb89ee9914bd4e281b4cc253ea0ac3bc4d150
Cibirsk <stvl@gmx.com>
2021-04-27 16:11:56 +0200

    Update EJS_test.js

--- EJS_test.js
+++ EJS_test.js
@@ -1,27 +1,11 @@
-/*function compteur() {
-    let count = 0;
-  
-    return function() {
-        return count++;
-    };
-}
-
-let plusUn = compteur();
-let plusUnBis = compteur();
-
-alert(plusUn()); //0 
-alert(plusUn()); //1
-alert(plusUnBis()); //0
-alert(plusUn()); //2
-alert(plusUnBis()); //1  */
-
-function creerFonction() {
-  var nom = "Mozilla";
-  function afficheNom() {
-    console.log(nom);
+function outerFunction(){
+  let outVar= "Stephane";
+  function innerFunction(){
+    let innerVar= outVar + "Sinclar";
+    console.log(innerVar);
   }
-  return afficheNom;
+  return innerFunction;
 }
 
-var maFonction = creerFonction();
-maFonction();
+let myVariable = outerFunction();
+myVariable();
